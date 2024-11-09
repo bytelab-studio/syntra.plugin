fdb6a56ef13f108304909d0133f48c9f09a43681
Christoph Koschel <christoph.koschel86@gmail.com>
2024-11-09 18:41:54 +0100

    New SQLTypes

--- src/SQLType.ts
+++ src/SQLType.ts
@@ -14,7 +14,13 @@ const enum SQLTypeGroup {
     INT,
     BIGINT,
 
+    FLOAT,
+    DOUBLE,
+
     VARCHAR,
+    DATE,
+    TIME,
+    DATETIME
 }
 
 export class SQLType {
@@ -23,6 +29,14 @@ export class SQLType {
     public static readonly MEDIUMINT: SQLType = new SQLType("MEDIUMINT", "number", SQLTypeGroup.MEDIUMINT);
     public static readonly INT: SQLType = new SQLType("INT", "number", SQLTypeGroup.INT);
     public static readonly BIGINT: SQLType = new SQLType("BIGINT", "number", SQLTypeGroup.BIGINT);
+    public static readonly BOOL: SQLType = new SQLType("TINYINT", "boolean", SQLTypeGroup.TINYINT);
+
+    public static readonly FLOAT: SQLType = new SQLType("FLOAT", "number", SQLTypeGroup.FLOAT);
+    public static readonly DOUBLE: SQLType = new SQLType("DOUBLE", "number", SQLTypeGroup.DOUBLE);
+
+    public static readonly DATE: SQLType = new SQLType("DATE", "string", SQLTypeGroup.DATE);
+    public static readonly TIME: SQLType = new SQLType("TIME", "string", SQLTypeGroup.TIME);
+    public static readonly DATETIME: SQLType = new SQLType("DATETIME", "string", SQLTypeGroup.DATETIME);
 
     public readonly sqlName: string;
     public readonly jsonType: string;
@@ -36,17 +50,27 @@ export class SQLType {
 
     public validate(value: any): boolean {
         if (this.group == SQLTypeGroup.TINYINT) {
-            return typeof value == "number" && math.inRange(value, -128, 127);
+            return typeof value == "number";
         } else if (this.group == SQLTypeGroup.SMALLINT) {
-            return typeof value == "number" && math.inRange(value, -32768, 32767);
+            return typeof value == "number";
         } else if (this.group == SQLTypeGroup.MEDIUMINT) {
-            return typeof value == "number" && math.inRange(value, -8388608, 8388607);
+            return typeof value == "number";
         } else if (this.group == SQLTypeGroup.INT) {
-            return typeof value == "number" && math.inRange(value, -2147483648, 2147483647);
+            return typeof value == "number";
         } else if (this.group == SQLTypeGroup.BIGINT) {
-            return typeof value == "number" && math.inRange(value, -Math.pow(2, 63), Math.pow(2, 63) - 1);
+            return typeof value == "number";
         } else if (this.group == SQLTypeGroup.VARCHAR) {
             return this instanceof VarChar && typeof value == "string" && value.length <= this.size;
+        } else if (this.group == SQLTypeGroup.FLOAT) {
+            return typeof value == "number";
+        } else if (this.group == SQLTypeGroup.DOUBLE) {
+            return typeof value == "number";
+        } else if (this.group == SQLTypeGroup.DATE) {
+            return value instanceof Date;
+        } else if (this.group == SQLTypeGroup.TIME) {
+            return value instanceof Date;
+        } else if (this.group == SQLTypeGroup.DATETIME) {
+            return value instanceof Date;
         } else if (this.group == SQLTypeGroup.NONE) {
             return true;
         } else {
